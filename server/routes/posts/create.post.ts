d1a9f077ffe3c41f8b35cf7bde8be820074122f3
Joey Fang <josudoey@gmail.com>
2024-12-01 23:10:25 +0800

    feat: add zod

--- server/routes/posts/create.post.ts
+++ server/routes/posts/create.post.ts
@@ -1,6 +1,22 @@
+import z from "zod";
 import { createPost } from "~/repository/post";
+const postForm = z.object({
+  name: z.string().trim().min(1, { message: "Required" }),
+  slug: z.string().trim().min(1, { message: "Required" }),
+  description: z.string().trim().min(1, { message: "Required" }),
+  color: z.string().trim().min(1, { message: "Required" }),
+  body: z.string().trim().min(1, { message: "Required" }),
+});
+
 export default defineEventHandler(async (event) => {
-  const body = await readBody(event);
-  const post = await createPost(body);
+  const result = postForm.safeParse(await readBody(event));
+  if (!result.success) {
+    setResponseStatus(event, 400);
+    return {
+      error: result.error,
+    };
+  }
+
+  const post = await createPost(result.data);
   return post;
 });
