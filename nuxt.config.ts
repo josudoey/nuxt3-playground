2d43c65dacaf3111a74c60843fdb0059333ca210
Joey Fang <josudoey@gmail.com>
2024-12-01 09:26:38 +0800

    feat: add redis count example

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,11 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-11-01',
-  devtools: { enabled: true }
-})
+  compatibilityDate: "2024-11-01",
+  devtools: { enabled: true },
+  runtimeConfig: {
+    redis: {
+      port: 6379,
+      host: "127.0.0.1",
+    },
+  },
+});
---
+++ server/routes/redis/count.ts
@@ -0,0 +1,9 @@
+export default defineEventHandler(async (event) => {
+  let count = parseInt((await useStorage("redis").getItem("count")) || "") || 0;
+  count += 1;
+  await useStorage("redis").setItem("count", count);
+
+  return {
+    count,
+  };
+});
